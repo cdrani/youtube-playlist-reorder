a21b668dc7364e92ca373fbe0198457d54806db3
cdrainxv <cdrainxv@users.noreply.github.com>
2018-08-26 20:00:43 -0600

    functionality to reverse playlist order

--- js/youtube.js
+++ js/youtube.js
@@ -1,1 +1,9 @@
-console.log('hello')
+const videoList = document.querySelectorAll('ytd-playlist-video-renderer')
+let parent = videoList[0].parentElement
+parent.innerHTML = ''
+
+const videos = Array.from(videoList).reverse()
+
+videos.forEach(video => {
+  parent.appendChild(video)
+})

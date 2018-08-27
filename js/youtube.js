749be7916f8427756fc05d96b9b80099cd2075c5
cdrainxv <cdrainxv@users.noreply.github.com>
2018-08-26 22:45:16 -0600

    ensure js file runs after dom loaded

--- js/youtube.js
+++ js/youtube.js
@@ -1,11 +1,11 @@
 const videoList = document.querySelectorAll('ytd-playlist-video-renderer')
+const videos = Array.from(videoList).reverse()
+let largeThumbnail = document.getElementById('playlist-thumbnails')
+let thumbnailImg = largeThumbnail.getElementsByTagName('img')[0]
 const parent = videoList[0].parentElement
 parent.innerHTML = ''
 
-const videos = Array.from(videoList).reverse()
-
 videos.forEach((video, index) => {
-  console.log(video)
   const numElement = video.getElementsByTagName('yt-formatted-string')[0]
   numElement.textContent = index + 1
   parent.appendChild(video)

2b52411969d2456c82c72b44b9b3dd620943316d
cdrainxv <cdrainxv@users.noreply.github.com>
2018-08-26 20:14:39 -0600

    reorder playlist numbers to be in correct order

--- js/youtube.js
+++ js/youtube.js
@@ -1,9 +1,12 @@
 const videoList = document.querySelectorAll('ytd-playlist-video-renderer')
-let parent = videoList[0].parentElement
+const parent = videoList[0].parentElement
 parent.innerHTML = ''
 
 const videos = Array.from(videoList).reverse()
 
-videos.forEach(video => {
+videos.forEach((video, index) => {
+  console.log(video)
+  const numElement = video.getElementsByTagName('yt-formatted-string')[0]
+  numElement.textContent = index + 1
   parent.appendChild(video)
 })
